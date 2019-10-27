961ce5c722f2d9919d36cab8fa3d374085d5eee6
wm <15871491953@163.com>
2019-10-27 14:26:55 +0800

    2019年10月27日提交

--- src/main/java/com/play/majiang/jus_demo/controller/PublicController.java
+++ src/main/java/com/play/majiang/jus_demo/controller/PublicController.java
@@ -1,12 +1,15 @@
 package com.play.majiang.jus_demo.controller;
 
+import com.play.majiang.jus_demo.dto.QuestionDTO;
 import com.play.majiang.jus_demo.mapper.QuestionMapper;
 import com.play.majiang.jus_demo.model.Question;
 import com.play.majiang.jus_demo.model.User;
+import com.play.majiang.jus_demo.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,25 +21,38 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublicController {
+
     @Autowired
-    private QuestionMapper questionMapper;
+    private QuestionService questionService;
 
-
+    @GetMapping("/publish/{id}")
+        public String edit(@PathVariable(name="id")Integer id,
+                Model model){
+            QuestionDTO question=questionService.getById(id);
+            model.addAttribute("title",question.getTitle());
+            model.addAttribute("description",question.getDescription());
+            model.addAttribute("tag",question.getTag());
+            model.addAttribute("id",question.getId());
+            return "publish";
+    }
 
      @GetMapping("/publish")
      public  String publish(){
-         return "publish";
+        return "publish";
      }
+
      @PostMapping("/publish")
      public  String doPublish(
              @RequestParam(value = "title",required = false) String title,
              @RequestParam(value = "description",required = false) String description,
              @RequestParam(value = "tag",required = false) String tag,
+             @RequestParam(value="id",required = false) Integer id,
              HttpServletRequest request,
              Model model) {
          model.addAttribute("title",title);
          model.addAttribute("description",description);
          model.addAttribute("tag",tag);
+
          if(title==null||title==""){
             model.addAttribute("error","标题不能为空");
             return "publish";
@@ -60,9 +76,8 @@ public class PublicController {
          question.setDescription(description);
          question.setTag(tag);
          question.setCreator(user.getId());
-         question.setGmtCreate(System.currentTimeMillis());
-         question.setGmtModified(question.getGmtCreate());
-         questionMapper.create(question);
+         question.setId(id);
+         questionService.createOrUpdate(question);
          return "redirect:/";
      }
 }
